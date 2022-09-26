bc64f242f4164e8c23314ee509b4ef3a67b7cc39
Pablo Galazzo <pablog_47@hotmail.com>
2022-09-26 16:10:33 -0300

    Pequeños cambios con css y uso de JSON

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-let cuotas = 1
+let cuotas = JSON.parse(localStorage.getItem("cuotas")) || 1
 const IVA = 1.21
 const INTERES = 1.05
 let total = 0
-const nuevoID = () => parseInt(Math.random() * 100000)
 const validarNumeroEnRango = (num, min, max) => !isNaN(num) && num >= min && num <= max
 const calcularPrecio = (precio, cuotas) => {
     const precioFinal = precio * IVA * (cuotas > 1 ? INTERES : 1)
     document.write(`El valor total de su compra (con IVA incluído) es de: $${precioFinal} en ${cuotas} cuota${cuotas > 1 ? `s de $${(precioFinal / cuotas).toFixed(2)}` : ""}.`)
 }
 
-let carrito = []
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 const buscador = document.getElementById("buscador")
 const listaProductos = document.getElementById("listaProductos")
 const carritoProductos = document.getElementById("carritoProductos")
@@ -19,52 +18,52 @@ const totalCompra = document.getElementById("totalCompra")
 const btnConfirmarCuotas = document.getElementById("confirmarCuotas")
 const productos = [
     {
-        id: nuevoID(),
+        id: 1545,
         nombre: "Persona 5 Royale",
         precio: 12000,
     },
     {
-        id: nuevoID(),
+        id: 1836,
         nombre: "Elden Ring",
         precio: 13000,
     },
     {
-        id: nuevoID(),
+        id: 1962,
         nombre: "Tales Of Arise",
         precio: 12500,
     },
     {
-        id: nuevoID(),
+        id: 1254,
         nombre: "Dark Souls Remaster",
         precio: 7000,
     },
     {
-        id: nuevoID(),
+        id: 1100,
         nombre: "GTA V",
         precio: 5000,
     },
     {
-        id: nuevoID(),
+        id: 1999,
         nombre: "Final Fantasy XVI (pre-order)",
         precio: 22000,
     },
     {
-        id: nuevoID(),
+        id: 1755,
         nombre: "Stray",
         precio: 9000,
     },
     {
-        id: nuevoID(),
+        id: 1423,
         nombre: "Nier: Replicant",
         precio: 11000,
     },
     {
-        id: nuevoID(),
+        id: 1025,
         nombre: "Scarlet Nexus",
         precio: 10000,
     },
     {
-        id: nuevoID(),
+        id: 1001,
         nombre: "Dragon Quest 11",
         precio: 10000,
     },
@@ -93,6 +92,7 @@ const agregarProducto = (numero) => {
         } else {
             carrito.push({ ...producto, cantidad: 1 })
         }
+        localStorage.setItem("carrito", JSON.stringify(carrito))
         renderizarCarrito()
     }
 }
@@ -104,6 +104,7 @@ const quitarProducto = (numero) => {
         cuotas = 1
         inputCuotas.value = ""
     }
+    localStorage.setItem("carrito", JSON.stringify(carrito))
     renderizarCarrito()
 }
 
@@ -112,9 +113,11 @@ const renderizarCarrito = () => {
     total = 0
     carrito.forEach((articulo, i) => {
         const item = document.createElement("li")
+        item.className = "m-2"
         const botonQuitar = document.createElement("button")
         botonQuitar.innerHTML = `Quitar`
         botonQuitar.type = "button"
+        botonQuitar.className = "btn btn-secondary"
         botonQuitar.addEventListener("click", () => quitarProducto(i))
         item.innerHTML = `<span>${articulo.nombre} - $${(articulo.precio * articulo.cantidad * IVA).toFixed(2)} (${articulo.cantidad} x $${articulo.precio} + IVA)</span>`
         item.appendChild(botonQuitar)
@@ -126,11 +129,11 @@ const renderizarCarrito = () => {
 
 const confirmarCuotas = () => {
     cuotas = parseInt(inputCuotas.value)
-    console.log(cuotas)
     if(!validarNumeroEnRango(cuotas, 1, 6)) {
         alert("Debe ingresar un número entre 1 y 6")
         cuotas = 1
     }
+    localStorage.setItem("cuotas", JSON.stringify(cuotas))
     mostrarTotal()
 }
 
@@ -148,9 +151,11 @@ const renderizarProductos = () => {
     if(productosFiltrados.length) {
         productosFiltrados.forEach((producto, i) => {
             const item = document.createElement("li")
+            item.className = "m-2"
             const botonAgregar = document.createElement("button")
             botonAgregar.innerHTML = `Agregar`
             botonAgregar.type = "button"
+            botonAgregar.className = "btn btn-secondary"
             botonAgregar.addEventListener("click", () => agregarProducto(i+1))
             item.innerHTML = `<span>${producto.nombre} - $${producto.precio} (${producto.id})</span>`
             item.appendChild(botonAgregar)
@@ -165,3 +170,4 @@ buscador.addEventListener("input", filtrarProductos)
 btnConfirmarCuotas.addEventListener("click", confirmarCuotas)
 
 filtrarProductos()
+renderizarCarrito()
